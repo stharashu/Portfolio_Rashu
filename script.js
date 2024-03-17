289e7ff23456ec127e2acbb1d5c99cf5e381d87a
stharashu <shrestha.rashu1@gmail.com>
2024-03-17 16:16:16 +0545

    Changes

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ function toggleMenu() {
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+
+}
+
+document.getElementById('dark-mode-checkbox').addEventListener('change', function() {
+  const body = document.body;
+  if (this.checked) {
+      body.classList.add('dark-mode');
+      document.getElementById('dark-mode-text').innerText = 'Light Mode';
+  } else {
+      body.classList.remove('dark-mode');
+      document.getElementById('dark-mode-text').innerText = 'Dark Mode';
   }
+});
+
