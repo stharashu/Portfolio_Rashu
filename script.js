590ba861e1c44695b8b0b5c2af51c0e199db0c3f
stharashu <shrestha.rashu1@gmail.com>
2024-03-22 21:15:37 +0545

    New edits

--- script.js
+++ script.js
@@ -5,15 +5,3 @@ function toggleMenu() {
     icon.classList.toggle("open");
 
 }
-
-document.getElementById('dark-mode-checkbox').addEventListener('change', function() {
-  const body = document.body;
-  if (this.checked) {
-      body.classList.add('dark-mode');
-      document.getElementById('dark-mode-text').innerText = 'Light Mode';
-  } else {
-      body.classList.remove('dark-mode');
-      document.getElementById('dark-mode-text').innerText = 'Dark Mode';
-  }
-});
-
