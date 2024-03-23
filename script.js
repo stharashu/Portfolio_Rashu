81f1b29418f7ae809f9c634c315346de2e39f9a9
stharashu <shrestha.rashu1@gmail.com>
2024-03-23 09:43:33 +0545

    form

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ function toggleMenu() {
     icon.classList.toggle("open");
 
 }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const darkModeToggle = document.getElementById("dark-mode-checkbox");
+  
+//     // Set initial theme based on user's preference (optional)
+//     if (localStorage.getItem("theme") === "dark") {
+//       document.body.classList.add("dark-mode");
+//       darkModeToggle.checked = true;
+//     }
+  
+//     // Event listener for dark mode toggle
+//     darkModeToggle.addEventListener("change", function() {
+//       if (darkModeToggle.checked) {
+//         document.body.classList.add("dark-mode");
+//         localStorage.setItem("theme", "dark"); // Save user's preference
+//       } else {
+//         document.body.classList.remove("dark-mode");
+//         localStorage.setItem("theme", "light"); // Save user's preference
+//       }
+//     });
+//   });
+  
+function toggleDarkMode() {
+    const body = document.body;
+    const icon = document.getElementById("dark-mode-icon");
+  
+    if (body.classList.contains("dark-mode")) {
+      body.classList.remove("dark-mode");
+      icon.src = "moon.png";
+      localStorage.setItem("theme", "light"); // Save user's preference
+    } else {
+      body.classList.add("dark-mode");
+      icon.src = "sun.png";
+      localStorage.setItem("theme", "dark"); // Save user's preference
+    }
+  }
+  
